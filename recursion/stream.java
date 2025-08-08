35d0a0d628ed11f930b7cb9d7ea069ba70c79247
saiganesheswaraprasad2004 <e.saiganesh@gmail.com>
2025-08-08 16:50:56 +0530

    Create stream.java

---
+++ recursion/stream.java
@@ -0,0 +1,36 @@
+package recursion;
+
+public class stream {
+    public static void main(String[] args) {
+        System.out.println(skip("baccdah"));
+    }
+
+    static void skip(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch=up.charAt(0);
+
+        if(ch=='a'){
+            skip(p,up.substring(1));
+        } else{
+            skip(p+ch,up.substring(1));
+        }
+    }
+
+    static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        char ch=up.charAt(0);
+
+        if(ch=='a'){
+            return skip(up.substring(1));
+        } else{
+            return ch+skip(up.substring(1));
+        }
+    }
+}
